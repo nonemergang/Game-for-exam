7144d173696d9ca6120fc7c00f23c76e65cb9757
Danil Mannanov <vildanmannanov2@gmail.com>
2025-04-19 23:30:23 +0500

    ...

---
+++ primitives.py
@@ -0,0 +1,12 @@
+import math
+
+class GameObject:
+    def __init__(self, game):
+        self.game = game
+
+    def update(self, dt, events):
+        raise NotImplementedError()
+
+    def draw(self, surf, offset = (0, 0)):
+        raise NotImplementedError()
+
