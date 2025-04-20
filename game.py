75e12c766c733f3eae5dbcdded0cedb2b7bf7bb4
Danil Mannanov <vildanmannanov2@gmail.com>
2025-04-20 09:40:50 +0500

    ...

--- game.py
+++ game.py
@@ -1,6 +1,41 @@
-def main():
-    pass
+import constants as c
+import pygame
+import sys
 
 
-if __name__ == '__main__':
-    main()
+
+
+
+class Game():
+    def __init__(self):
+        pygame.init()
+        if c.FULLSCREEN:
+            self.screen = pygame.display.set_mode(c.WINDOW_SIZE, flags = pygame.FULLSCREEN)
+        else:
+            self.screen = pygame.display.set_mode(c.WINDOW_SIZE)
+
+        self.clock = pygame.time.Clock()
+        pygame.mouse.set_visible(False)
+
+
+    def get_events(self):
+        dt = self.clock.tick(c.FRAMERATE)/1000
+
+
+        events = pygame.event.get()
+        for event in events:
+            if event.type == pygame.QUIT:
+                pygame,quit()
+                sys.exit()
+
+
+        return dt, events
+
+
+if __name__ =="__main__":
+    game = Game()
+    game.main()
+
+
+
+
